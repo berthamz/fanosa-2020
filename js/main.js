964f9cc4c0b83605787674cea88c27d197639218
berthamz <berthamz@gmail.com>
2020-05-15 20:03:51 -0500

    Fonts imported on sass files

--- js/main.js
+++ js/main.js
@@ -30,4 +30,32 @@ $(document).ready(function () {
       }
     });
   });
+
+  // Overlay Descargas
+
+  $(".js-descargas-open").click(function () {
+    $("body").addClass("noscroll");
+    $(".overlay-descargas").fadeIn(100);
+  });
+
+  // Overlay Galerías y Videos
+
+  $(".base-grid > div, .overlay-categorias-btns > div").click(function () {
+    var overlayName = this.className;
+    $("body").addClass("noscroll");
+
+    if ($(this).parents(".overlay-wrapper").length) {
+      $(".overlay-base").fadeOut(200);
+      $(".overlay-" + overlayName).fadeIn(200);
+    } else {
+      $(".overlay-" + overlayName).fadeIn(200);
+    }
+  });
+
+  // Botón Close para Descargas, Galerías y Videos
+
+  $(".js-close").click(function () {
+    $("body").removeClass("noscroll");
+    $(".overlay-base").fadeOut(100);
+  });
 });
